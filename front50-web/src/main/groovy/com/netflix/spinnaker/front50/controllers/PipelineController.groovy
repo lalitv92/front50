7bd5eb80a96500590098750aeb5c907d46284542
j-sandy <jsandy>
2018-11-13 16:43:42 +0530

    added isAdmin() in preauthorize for save()

--- front50-web/src/main/groovy/com/netflix/spinnaker/front50/controllers/PipelineController.groovy
+++ front50-web/src/main/groovy/com/netflix/spinnaker/front50/controllers/PipelineController.groovy
@@ -94,7 +94,7 @@ class PipelineController {
     return pipelineDAO.history(id, limit)
   }
 
-  @PreAuthorize("@fiatPermissionEvaluator.storeWholePermission() and hasPermission(#pipeline.application, 'APPLICATION', 'WRITE') and @authorizationSupport.hasRunAsUserPermission(#pipeline)")
+  @PreAuthorize("(@fiatPermissionEvaluator.storeWholePermission() and hasPermission(#pipeline.application, 'APPLICATION', 'WRITE') and @authorizationSupport.hasRunAsUserPermission(#pipeline)) or @fiatPermissionEvaluator.isAdmin()")
   @RequestMapping(value = '', method = RequestMethod.POST)
   Pipeline save(@RequestBody Pipeline pipeline) {
 
